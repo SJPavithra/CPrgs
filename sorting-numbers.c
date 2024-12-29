4c0b199cc08f46f20016e16cee68e15a9056406d
SJPavithra <22bca079@psgrkcw.ac.in>
2024-12-29 16:01:39 +0530

    Create sorting-numbers.c

---
+++ sorting-numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int arr[] = {5, 2, 9, 1, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]), temp;
+
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}
